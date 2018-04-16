593dbf086d42ee1cc259ca7090ef148029b90d7e
rubenbelem <rubenbelem@gmail.com>
2018-04-16 09:46:39 -0400

    Visual do JS4 melhorado

--- JS4/scripts/script.js
+++ JS4/scripts/script.js
@@ -4,10 +4,11 @@ var circunference;
 
 let calculate = function() {
 	radius = parseFloat(document.getElementsByName('radiusInput')[0].value);
-	area = Math.PI * radius * radius;
+	area = Math.PI * Math.pow(radius ,2);
 	circunference = 2 * Math.PI * radius;
 	document.getElementsByName('areaInput')[0].value = area.toFixed(2);
-	document.getElementsByName('circunferenceInput')[0].value = circunference.toFixed(2);
+	let circInput = document.getElementsByName('circunferenceInput')[0];
+	circInput.value = circunference.toFixed(2);
 };
 
 let keyPress = function(key) {
