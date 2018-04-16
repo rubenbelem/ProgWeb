b8986e9a7ab8db089cbfa405ecc5f9e29b7dc276
rubenbelem <rubenbelem@gmail.com>
2018-04-16 09:27:53 -0400

    Exercício JS4 melhorado com tecla enter

--- JS4/scripts/script.js
+++ JS4/scripts/script.js
@@ -1,8 +1,16 @@
-let calculate = function() {
-	let radius = parseFloat(document.getElementsByName('radiusInput')[0].value);
-	let area = Math.PI * radius * radius;
-	let circunference = 2 * Math.PI * radius;
+var area;
+var radius;
+var circunference;
 
+let calculate = function() {
+	radius = parseFloat(document.getElementsByName('radiusInput')[0].value);
+	area = Math.PI * radius * radius;
+	circunference = 2 * Math.PI * radius;
 	document.getElementsByName('areaInput')[0].value = area.toFixed(2);
 	document.getElementsByName('circunferenceInput')[0].value = circunference.toFixed(2);
 };
+
+let keyPress = function(key) {
+	if (key.code == "Enter")
+		calculate();
+}
