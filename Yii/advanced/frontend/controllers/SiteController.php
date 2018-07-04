82da9452a810192cf2f352cc018c7796ea9c3afd
Rúben Belém <rubenbelem@gmail.com>
2018-07-04 10:58:38 -0400

    Exercício 11

--- Yii/advanced/frontend/controllers/SiteController.php
+++ Yii/advanced/frontend/controllers/SiteController.php
@@ -5,6 +5,7 @@ use Yii;
 use yii\base\InvalidParamException;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
+
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use common\models\LoginForm;
--- Yii/advanced/frontend/views/site/index.php
+++ Yii/advanced/frontend/views/site/index.php
@@ -1,16 +1,18 @@
 <?php
 
 /* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+$this->title = 'Instituto de Computação';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <?= Html::img('@web/img/icomp.png',['width'=>'400']) ?>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead"><br/><br/>O Instituto de Computação (IComp), antigo Departamento de Ciência da Computação (DCC),  é um instituto acadêmico que agrega os professores da área de computação. Como todo instituto acadêmico o IComp atua no ensino, pesquisa e extensão, além de desempenhar atividades administrativas.
 
+            No ensino de graduação, o DCC atua principalmente nos cursos de Ciência da Computação, Sistemas de Informação, Engenharia de Software e Engenharia da Computação. Também oferece disciplinas básicas de computação para 30 cursos de graduação da UFAM em Manaus.</p>
+        <br/>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
